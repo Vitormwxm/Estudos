ff8a08a171a8c138aeb5a579a4ab8cda9e362465
Vitor Martiniano <vitormwxm@gmail.com>
2024-08-23 09:48:44 -0300

    refactor: alterando código do JS do HTML para o arquivo script.js

--- desafios/bingo/script.js
+++ desafios/bingo/script.js
@@ -1,9 +1,9 @@
+let btn = document.getElementById("buttonRadom");
+
+btn.addEventListener("click", numberRadom);
 
 function numberRadom(  ) {
     let modificador = document.getElementById("number")
-    let div = document.querySelector(".ball")
-
-    
 
     modificador.innerText = Math.floor(Math.random(modificador) * 61) 
 }
