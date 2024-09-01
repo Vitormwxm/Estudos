9b48012a74fcb7ad2ea79b6ae30554f3ea8a48c8
Vitor Martiniano <vitormwxm@gmail.com>
2024-09-01 17:43:11 -0300

    aula de arrays

--- descomplica/arrays.js
+++ descomplica/arrays.js
@@ -12,3 +12,18 @@ let segundosBimestre = terceiroTri.concat(quartorTri);
 
 console.log(primerioBimestre)
 console.log(segundosBimestre)
+
+segundosBimestre.unshift("onzembro")
+
+console.log(segundosBimestre)
+
+var numeros = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+
+function arryfilter(value){
+    if(value % 2 == 0 )
+    return value
+}
+
+var pares = numeros.filter(arryfilter);
+
+console.log(pares)
